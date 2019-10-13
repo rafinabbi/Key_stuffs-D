7b950492f5aae6f616aaec390969a3499c3aa2b5
rafinabbi <54761115+rafinabbi@users.noreply.github.com>
2019-10-13 18:59:02 +0600

    Update ChatAt_Count.java

--- ChatAt_Count.java
+++ ChatAt_Count.java
@@ -1,7 +1,6 @@
 import java.util.*;
+
 public class ChatAt_Count {
-
-
         public static void main (String []args){
             Scanner s = new Scanner(System.in);
             int [] myArray = new int[256];
@@ -10,14 +9,10 @@ public class ChatAt_Count {
             for(int i=0;i<a.length();i++){
                 myArray[a.charAt(i)]++;
             }
-
-
             for(int i=0;i<256;i++){
                 if(myArray[i]!=0){
                     System.out.println((char)i+"="+myArray[i]);
                 }
             }
-
     }
-
 }
